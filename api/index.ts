4605396b95f35dff03b5926609ea5a925acef5d3
Nazareno Soto <ceka.dora@gmail.com>
2024-12-14 15:42:06 -0300

    turso db implementation

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const turso = require('./turso_db')
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/main.html'))
@@ -9,6 +10,10 @@ app.get("/", (req, res) => {
 app.get("/test", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/test.html'))
 });
+
+app.get("/users", (req, res) => {
+    turso.execute('SELECT * FROM users')
+})
 
 
 app.listen(8080, () => console.log("Server ready on port 3000."));
