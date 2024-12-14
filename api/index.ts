211e49107be37ccf8c885a05ea7966b29c34e17c
Nazareno Soto <ceka.dora@gmail.com>
2024-12-14 00:16:43 -0300

    test

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.sendFile(
+    '../public/main.html', {root: __dirname }
+));
+
+app.get("/test", (req, res) => res.send("test"));
+
 
 app.listen(8080, () => console.log("Server ready on port 3000."));
 
