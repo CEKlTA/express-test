479f39e1235a9863db7904f348a2442ea72c8958
Nazareno Soto <ceka.dora@gmail.com>
2024-12-14 00:23:00 -0300

    test

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 
-app.get("/", (req, res) => res.sendFile(
-    '../public/main.html', {root: __dirname }
-));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/main.html'))
+});
 
-app.get("/test", (req, res) => res.send("test"));
+app.get("/test", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/test.html'))
+});
 
 
 app.listen(8080, () => console.log("Server ready on port 3000."));
